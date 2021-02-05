a593e4cdfffd0b89cb7492cbd2b5413559ccc25e
mkeyer <mcckeyer002@outlook.com>
2021-02-04 21:37:10 -0500

    big_1

---
+++ clockdegree.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+float degree(int hour, int min)
+{
+  //angle_hour - angle_min
+  float angle_hour = hour * 30;
+  angle_hour += min * 0.5;
+  printf("angle_hour: %f\n", angle_hour);
+  float angle_min = min * 6;
+  float res = fabs(angle_hour - angle_min);
+  if (res > 180) {
+    res = 360 - res;
+  }
+  return res;
+}
+
+int main(int argc, char *argv[])
+{
+  printf("%f\n", degree(1, 5));
+  
+  return 0;
+}
